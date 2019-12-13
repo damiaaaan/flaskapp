d9389203441fd52ae449357c8b2123ad659e9229
Damian Bertoni <damianberto@gmail.com>
2019-12-13 14:44:51 -0300

    feat: flask-uploads config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
@@ -13,14 +14,13 @@ class Config(object):
     APP_MAIL_SUBJECT_PREFIX = 'FLASKAPP-MAIL'
     APP_MAIL_SENDER = 'SENDER'
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-        ['true', 'on', '1']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     APP_MAIL_SUBJECT_PREFIX = '[FlaskApp]'
-    APP_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    APP_MAIL_SENDER = 'FlaskApp Admin <flasky@example.com>'
     APP_ADMIN = os.environ.get('FLASK_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -30,6 +30,9 @@ class Config(object):
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
     FLASKY_SLOW_DB_QUERY_TIME = 0.5
+
+    UPLOADED_AVATARS_DEST = os.path.join(basedir, 'app/static/image')
+
 
     @staticmethod
     def init_app(app):
