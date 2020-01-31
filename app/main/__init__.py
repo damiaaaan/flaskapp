27bacf153f4bc2ac3e463b1ae0214d0cc65a0e46
Damian Bertoni <damianberto@gmail.com>
2020-01-31 13:59:08 -0300

    feat: inject Permissions to the app, menu for administrators

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
+
 bp = Blueprint('main', __name__)
 
 from app.main import routes
+from app.models import Permission
+
+@bp.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
