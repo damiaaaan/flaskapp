e6a800c34267a113494e1e46ba56b8e23adcdf29
Damian Bertoni <damianberto@gmail.com>
2020-02-10 14:20:39 -0300

    fix: Change in configuration

--- flaskapp.py
+++ flaskapp.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_ENV') or 'default')
